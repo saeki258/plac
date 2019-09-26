c118a6f65113ef44bb6d2875f181808754f14f75
r_saeki <saeki.r@78it.com>
2019-09-26 15:09:01 +0900

    78test

--- service-worker.js
+++ service-worker.js
@@ -36,14 +36,15 @@ self.addEventListener('notificationclick', function(event) {
   event.waitUntil(clients.matchAll({
     type: 'window'
   }).then(function(clientList) {
-    for (var i = 0; i < clientList.length; i++) {
-      var client = clientList[i];
-      if (client.url === '/' && 'focus' in client) {
-        return client.focus();
-      }
-    }
-    if (clients.openWindow) {
-      return clients.openWindow('https://www.78it.com');
-    }
+    location.href = 'https://www.78it.com/';
+    // for (var i = 0; i < clientList.length; i++) {
+    //   var client = clientList[i];
+    //   if (client.url === '/' && 'focus' in client) {
+    //     return client.focus();
+    //   }
+    // }
+    // if (clients.openWindow) {
+    //   return clients.openWindow('/');
+    // }
   }));
 });
